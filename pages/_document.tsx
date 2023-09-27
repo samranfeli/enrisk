dace0ddf4d7de53390157dc98b9c895c2ad17f43
Samran Feliimoghadam <s.feli@Itours.local>
2023-09-27 12:20:37 +0330

    remove unused assets --- add favicon --- add site title, meta description an keywords and author

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Enrisk</title>
+        <meta name="description" content="Risk Based Decision Analysis" />
+        <meta name="keywords" content="Enrisk,Risk Based Decision Analysis,risk analysis" />
+        <meta name="author" content="Samran feli moghadam" />
+      </Head>
       <body className='md:base-font'>
         <Main />
         <NextScript />
