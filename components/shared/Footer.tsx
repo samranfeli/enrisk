87340c21f44c6f6b62bff534c133741febf47479
Samran Feliimoghadam <s.feli@Itours.local>
2023-09-27 13:05:12 +0330

    decrease footer height in mobile

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer : React.FC = () => {
     return(
-        <footer className="py-10 bg-stone-100 text-stone-400">
+        <footer className="py-5 md:py-10 bg-stone-100 text-stone-400">
             <div className="max-w-7xl  mx-auto px-3">
                 <p>© 2023 Enriskconsulting.com All Rights Reserved</p>
             </div>
