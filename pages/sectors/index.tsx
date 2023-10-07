ddb5ab70eab8b656c8b1c64ce1730908ae9adc6b
Samran Feliimoghadam <s.feli@Itours.local>
2023-10-08 00:48:59 +0330

    implement new design for sectors list

--- pages/sectors/index.tsx
+++ pages/sectors/index.tsx
@@ -18,20 +18,20 @@ const Sectors: React.FC<Props> = props => {
 
     return (
         <Layout>
-            <div className='max-w-7xl mx-auto px-3 py-4 lg:py-8 sm:text-justify'>
+            <div className='max-w-7xl mx-auto px-3 py-4 lg:py-8'>
                 <h1 className='font-bold text-xl xl:text-3xl mb-4 lg:mb-8 text-center'>Sectors</h1>
-                <div className="sm:flex flex-wrap justify-center">
+                <div className="sm:flex flex-wrap">
                     {sectors.map(sector => (
-                        <div className="p-3 lg:mb-5 shrink-0 sm:basis-1/2 lg:basis-1/3" key={sector.title}>
-                            <Link href={sector.url} className="relative block transition-all duration-300 xl:scale-100 xl:hover:scale-105">
-                                <Image
-                                    src={sector.thumbnailUrl}
-                                    alt={sector.title}
-                                    width={300}
+                        <div className="p-3 shrink-0 sm:basis-1/2 lg:basis-1/3" key={sector.title}>
+                            <Link href={sector.url} className="relative block group overflow-hidden box-shadow">
+                                <Image 
+                                    src={sector.thumbnailUrl} 
+                                    alt={sector.title} 
+                                    width={300} 
                                     height={100}
-                                    className="rounded-xl mb-2 mx-auto"
+                                    className="mb-2 w-full scale-100 group-hover:scale-105 transition-all duration-300 ease-in-out"
                                 />
-                                <h3 className="text-center font-semibold text-sky-600">
+                                <h3 className="font-semibold text-amber-500 absolute bottom-0 left-0 right-0 p-3 pt-20 bg-gradient-to-t from-black to-transparent">
                                     {sector.title}
                                 </h3>
                             </Link>
@@ -54,6 +54,5 @@ export async function getStaticProps(): Promise<GetStaticPropsResult<Props>> {
     };
 
 }
-
 
 export default Sectors;
