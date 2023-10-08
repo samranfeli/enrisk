5b97817ed04e120f760bc27a4b48e9c7338d036f
Samran Feliimoghadam <s.feli@Itours.local>
2023-10-08 12:07:55 +0330

    improve home banner call to action button  position

--- components/home/Banner.tsx
+++ components/home/Banner.tsx
@@ -9,7 +9,7 @@ export default function Banner() {
           <span className="inline-block bg-cyan-950/40 px-4 py-1 border-l-4 border-amber-500">Industrial Risk Management</span>
           <h2 className="text-4xl md:text-6xl font-bold my-3 md:my-4">Risk Based  Decision Anlysis</h2>
           <p className="text-lg mb-4 lg:mb-6">EnRisk addresses the complex challenges of its clients’ mega projects and develops strategies to manage their risks.</p>
-          <Link href={'/services'} className="btn red-btn block md:inline-block mb-10 lg:mb-3" >
+          <Link href={'/services'} className="btn red-btn block md:inline-block mb-14 lg:mb-3" >
             Explore More
           </Link>
         </div>
